67b28e646fd65981951fe0a1e097203c93df3dae
Makto Maktavish <66353680+Makrine@users.noreply.github.com>
2021-10-01 15:32:18 +0400

    Update ns.bat

--- Windows/ns.bat
+++ Windows/ns.bat
@@ -3,9 +3,9 @@
 setlocal
 
 
-SET USERAGENT=
-SET NATION1=
-SET PASSWORD1=
+SET USERAGENT=YourUserAgent
+SET NATION1=YourNationName
+SET PASSWORD1=YourNationPassword
 
 >nul curl -f -H "X-Password: %PASSWORD1%" -A "%USERAGENT%" "https://www.nationstates.net/cgi-bin/api.cgi?nation=%NATION1%&q=ping" && (
 	echo SUCCESS
